2d155206b5fbddd5a083d409d0ef99fd48a89680
Promise <promiseihunna@gmail.com>
2026-09-04 14:57:44 +0100

    chore: add config loader and project dependency

---
+++ internal/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"log/slog"
+	"os"
+
+	dotenv "github.com/joho/godotenv"
+)
+
+type Config struct {
+	DatabaseURL string
+	JWTSecret   string
+	Port        string
+}
+
+func Load(databaseURL string) (*Config, error) {
+	var err error
+	err = dotenv.Load()
+	if err != nil {
+		slog.Error("Warning: ", "message", ".env file not found "+err.Error())
+		return nil, err
+	}
+
+	var config *Config = &Config{
+		DatabaseURL: os.Getenv("DATABASE_URL"),
+		JWTSecret:   os.Getenv("JWT_SECRET"),
+		Port:        os.Getenv("PORT"),
+	}
+
+	return config, err
+}
